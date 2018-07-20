19fda5a620b4ff73ea47a3603acd41e9b6a1b1b3
HolgerVidal <holgervidal91@gmail.com>
2018-07-20 17:58:43 -0500

    Corregido

--- routes/web.php
+++ routes/web.php
@@ -10,11 +10,6 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/ventanaeder', 'HomeController@eder')->name('eder');
-
-/* Route::get('/', function () {
-    return view('ventanasInicio.ventanaInicial');
-})->name('inicio'); */
 Route::get('/listaContenidos','InicialConroller@todo');
 Route::get('/','InicialConroller@index')->name('inicio');
 
@@ -26,10 +21,9 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/administrar_info_eder','InformacionEderController@index')->name('administrar_info_eder');
 Route::resource('/gestionformacioneder','InformacionEderController');
 Route::put('/gestionformacioneder_actulizar','InformacionEderController@actualizar');
-
 Route::put('/gestiontitulos', 'InformacionEderController@actualizartitulos'); // tuta para actualizar el contenido de los titulos
 
-
+Route::get('/foros/lista','ForosController@showForos')->name('listaforos');
 // de esta manera tambian funciona solo que no la uso porque me dapereza crear cada una de las tinas jajajaja
 /* Route::post('/gestionformacioneder','InformacionEderController@ingresar'); */
 /* Route::get('/administrar_info_eder', function () {
